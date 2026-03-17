93a991aee85a26498c2a472b78b7ad873aff7001
mugnest <111188932+Mugnest531@users.noreply.github.com>
2026-03-17 22:23:04 +0500

    Delete src/main directory

--- src/main/java/ru/lewhu/extracttitles/service/effect/AppliedEffect.java
+++
@@ -1,8 +0,0 @@
-package ru.lewhu.extracttitles.service.effect;
-
-import ru.lewhu.extracttitles.domain.title.TitleDefinition;
-
-import java.util.Map;
-
-public record AppliedEffect(String titleId, TitleDefinition title, TitleEffect effect, Map<String, Object> options) {
-}
--- src/main/java/ru/lewhu/extracttitles/service/effect/EffectContext.java
+++
@@ -1,7 +0,0 @@
-package ru.lewhu.extracttitles.service.effect;
-
-import org.bukkit.plugin.java.JavaPlugin;
-import ru.lewhu.extracttitles.service.PermissionGrantService;
-
-public record EffectContext(JavaPlugin plugin, PermissionGrantService permissionGrantService, boolean reapply) {
-}
